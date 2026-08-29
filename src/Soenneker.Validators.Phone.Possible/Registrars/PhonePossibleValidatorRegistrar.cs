84f535e138c415b55702273835f57850b6698040
soenneker <jake@soenneker.com>
2026-08-29 14:17:27 -0500

    Improve public API XML documentation

--- src/Soenneker.Validators.Phone.Possible/Registrars/PhonePossibleValidatorRegistrar.cs
+++ src/Soenneker.Validators.Phone.Possible/Registrars/PhonePossibleValidatorRegistrar.cs
@@ -13,6 +13,8 @@ public static class PhonePossibleValidatorRegistrar
     /// <summary>
     /// Adds <see cref="IPhonePossibleValidator"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddPhonePossibleValidatorAsSingleton(this IServiceCollection services)
     {
         services.AddLibphonenumberUtilAsSingleton().TryAddSingleton<IPhonePossibleValidator, PhonePossibleValidator>();
@@ -23,6 +25,8 @@ public static class PhonePossibleValidatorRegistrar
     /// <summary>
     /// Adds <see cref="IPhonePossibleValidator"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddPhonePossibleValidatorAsScoped(this IServiceCollection services)
     {
         services.AddLibphonenumberUtilAsSingleton().TryAddScoped<IPhonePossibleValidator, PhonePossibleValidator>();
